6c2dff855b82e7b19d34bee6cdc6c9298b332441
Renato da Silva Viana <verkylen@gmail.com>
2022-08-25 01:16:55 -0300

    Refresh chat

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-let User = prompt('Digite seu nome:');
+let User = prompt('Digite seu nome de usuário:');
 let Post = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name: User});
-
-function Sucess(Response) {
-    console.log('Ok');
-    console.log(Response.data);
-}
 
 function Fail(Response) {
     console.log(Response);
     console.log("Status code: " + Response.response.status);
 	console.log("Mensagem de erro: " + Response.response.data);
 
-    User = prompt('Infelizmente, esse nome já está em uso. Digite outro nome:');
+    User = prompt('Infelizmente, esse nome já está em uso. Tente outro nome:');
     Post = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name: User});
 
     Post.then(Sucess);
     Post.catch(Fail);
 }
 
-Post.then(Sucess);
 Post.catch(Fail);
 
-function Refresh() {
+function Active() {
     axios.post('https://mock-api.driven.com.br/api/v6/uol/status', {name: User});
-    console.log('Refresh');
 }
 
-setInterval(Refresh, 5000);
+setInterval(Active, 5000);
+
+let Request;
+
+function Refresh() {
+    Request = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    Request.then(Update);
+}
+
+Refresh();
+
+function Update(Response) {
+    for (let i = 0; i < (Response.data).length; i++) {
+        let From = Response.data[i].from;
+        let To = Response.data[i].to;
+        let Text = Response.data[i].text;
+        let Type = Response.data[i].type;
+        let Time = Response.data[i].time;
+
+        switch(Type) {
+            case 'status':
+                document.querySelector('.Container').innerHTML += `<div class="System_message"><time>${Time}</time><strong>${From}</strong> ${Text}</div>`;
+                break;
+            
+            case 'message':
+                document.querySelector('.Container').innerHTML += `<div class="Public_message"><time>${Time}</time><strong>${From}</strong> para <strong>${To}</strong><span>:</span>${Text}</div>`;
+                break;
+
+            case 'private_message':
+                document.querySelector('.Container').innerHTML += `<div class="Private_message"><time>${Time}</time><strong>${From}</strong> reservadamente para <strong>${To}</strong><span>:</span>${Text}</div>`;
+                break;
+        }
+    }
+}
+
+setInterval(Refresh, 3000);
