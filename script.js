aabefc95c3dcf621a325981e2765b3773504447c
Renato da Silva Viana <verkylen@gmail.com>
2022-08-27 19:31:43 -0300

    Partial Javascript 1

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 let User = prompt('Digite seu nome de usuário:');
-let Post = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name: User});
+let Login = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name: User});
 
 function Fail(Response) {
+    console.log('Fail')
     console.log(Response);
     console.log("Status code: " + Response.response.status);
 	console.log("Mensagem de erro: " + Response.response.data);
 
     User = prompt('Infelizmente, esse nome já está em uso. Tente outro nome:');
-    Post = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name: User});
 
-    Post.then(Sucess);
-    Post.catch(Fail);
+    Login = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name: User});
+    Login.then(Refresh);
+    Login.catch(Fail);
 }
 
-Post.catch(Fail);
+Login.then(Refresh);
+Login.catch(Fail);
 
 function Active() {
     axios.post('https://mock-api.driven.com.br/api/v6/uol/status', {name: User});
@@ -21,47 +23,84 @@ function Active() {
 
 setInterval(Active, 5000);
 
-function Errormessage(x) {
-    console.log(x);
+function ErrorRequest(Response) {
+    console.log('ErrorRequest');
+    console.log(Response);
 }
 
+let Run = true;
+let IdInterval;
 let Request;
 
 function Refresh() {
     Request = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    Request.then(Update);
-    Request.catch(Errormessage);
+    Request.then(Load);
+    Request.catch(ErrorRequest);
+
+    if (Run) {
+        IdInterval = setInterval(Refresh, 3000);
+        Run = false;
+    }
 }
 
-function Update(Response) {
+
+let From;
+let To;
+let Txt;
+let Type;
+let Time;
+
+let View;
+
+function Load(Response) {
     for (let i = 0; i < (Response.data).length; i++) {
-        let From = Response.data[i].from;
-        let To = Response.data[i].to;
-        let Text = Response.data[i].text;
-        let Type = Response.data[i].type;
-        let Time = Response.data[i].time;
+        From = Response.data[i].from;
+        To = Response.data[i].to;
+        Txt = Response.data[i].text;
+        Type = Response.data[i].type;
+        Time = Response.data[i].time;
 
         switch(Type) {
             case 'status':
-                document.querySelector('.Container').innerHTML += `<div class="System_message"><time>${Time}</time><strong>${From}</strong> ${Text}</div>`;
+                document.querySelector('.Container').innerHTML += `<div class="System_message"><time>${Time}</time><strong>${From}</strong> ${Txt}</div>`;
                 break;
             
             case 'message':
-                document.querySelector('.Container').innerHTML += `<div class="Public_message"><time>${Time}</time><strong>${From}</strong> para <strong>${To}</strong><span>:</span>${Text}</div>`;
+                document.querySelector('.Container').innerHTML += `<div class="Public_message"><time>${Time}</time><strong>${From}</strong> para <strong>${To}</strong><span>:</span>${Txt}</div>`;
                 break;
 
             case 'private_message':
-                if (To == User) {
-                    document.querySelector('.Container').innerHTML += `<div class="Private_message"><time>${Time}</time><strong>${From}</strong> reservadamente para <strong>${To}</strong><span>:</span>${Text}</div>`;
+                if (From == User || To == User) {
+                    document.querySelector('.Container').innerHTML += `<div class="Private_message"><time>${Time}</time><strong>${From}</strong> reservadamente para <strong>${To}</strong><span>:</span>${Txt}</div>`;
                 }
                 break;
         }
     }
 
-    const View = document.querySelector('.Container div:last-child');
+    View = document.querySelector('.Container div:last-child');
     View.scrollIntoView();
 }
 
-Refresh();
+function Clear() {
+    document.querySelector('textarea').value = '';
+    clearInterval(IdInterval);
+    Run = true;
+    Refresh();
+}
 
-setInterval(Refresh, 3000);
+let Post;
+let Message;
+
+function ErrorPost(Response) {
+    console.log('ErrorPost');
+    console.log(Response);
+}
+
+function Enter() {
+    Message = document.querySelector('textarea').value;
+
+    Post = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', {from: User, to: 'Rainha do zapzap', text: Message, type:'private_message'});
+    Post.then(Clear);
+    Post.catch(ErrorPost);
+}
+
